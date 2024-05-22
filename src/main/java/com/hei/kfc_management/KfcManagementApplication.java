273c659c32e4ae8fefb71f56c95792513663d9b2
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-05-22 08:39:27 +0300

    initialize project

---
+++ src/main/java/com/hei/kfc_management/KfcManagementApplication.java
@@ -0,0 +1,13 @@
+package com.hei.kfc_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KfcManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KfcManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hei/kfc_management/KfcManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hei.kfc_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KfcManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
